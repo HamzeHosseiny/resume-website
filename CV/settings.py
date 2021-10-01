f432ed1892a52711c474783016a2a1ddc4a0fd53
hamze <hamzehosseiny@yahoo.com>
2021-10-01 13:57:20 +0330

    fderfer

--- CV/settings.py
+++ CV/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-mkm1z$f^=#uf$&ec0f%5p4!*ske4g%lbt93-4%+sv8+zt88zvd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://hamziooo.herokuapp.com/']
 
 
 # Application definition
